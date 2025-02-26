0ef7b4939274304f19691e8a0fa2aaba6c080f55
Jason Day <movemaine@gmail.com>
2025-02-26 11:54:28 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ module.exports = {
                 primary: themeStyle.primary
             },
             fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-                serif: ['Roboto Slab', 'serif']
+                sans: ['Roboto', 'sans-serif'],
+                serif: ['Literata', 'serif']
             },
             gridTemplateColumns: {
                 16: 'repeat(16, minmax(0, 1fr))'
