227d4a747a3ebdbaee3f3451f494f28fff35cedf
Jason Day <movemaine@gmail.com>
2025-02-27 10:38:42 -0500

    Update menu.tsx

--- src/components/svgs/menu.tsx
+++ src/components/svgs/menu.tsx
@@ -5,5 +5,11 @@ export default function Menu({ className, dynamicAttributes, ...props }) {
         <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-sb-field-path={props['data-sb-field-path']} {...dynamicAttributes} >
             <path d="M1.695 18.625v-1.767h20.611v1.767zM1.695 12.883v-1.767h20.611v1.767zM1.695 7.142v-1.767h20.611v1.767z"></path>
         </svg>
+
+        <div class="menu-hamburger" {...dynamicAttributes}>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
     );
 }
