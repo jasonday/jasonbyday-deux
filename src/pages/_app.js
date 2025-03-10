f264370517903767db8dc49e6dca7f2c9ea221df
Jason Day <movemaine@gmail.com>
2025-03-10 11:22:56 -0400

    adding postHog

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,36 @@
+// pages/_app.tsx
+import { useEffect } from 'react'
+import { Router } from 'next/router'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import type { AppProps } from 'next/app'
+
 import '../css/main.css';
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+
+    const handleRouteChange = () => posthog?.capture('$pageview')
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [])
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  )
 }
